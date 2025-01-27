199729f9a3faa0129f0201646077fac91a4a4980
jcardus <asklocation.net@gmail.com>
2025-01-27 00:48:16 +0000

    :

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ until curl --silent --fail -o /dev/null http://localhost; do
   echo "Waiting for Roundcube to be ready..."
   sleep 2  # Wait for 2 seconds before retrying
 done
-docker cp ./roundcube/config.php roundcube:/var/www/html/plugins/roundcube_oidc/config.inc.php:  
+docker cp ./roundcube/config.php roundcube:/var/www/html/plugins/roundcube_oidc/config.inc.php 
