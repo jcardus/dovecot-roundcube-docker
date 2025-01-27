c12fdbfa4d99e1e950b9e7b6093e131625ef7ef0
jcardus <asklocation.net@gmail.com>
2025-01-27 00:39:40 +0000

    --silent

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker compose up -d
-until curl --fail -o /dev/null http://localhost; do
+until curl --silent --fail -o /dev/null http://localhost; do
   echo "Waiting for Roundcube to be ready..."
   sleep 2  # Wait for 2 seconds before retrying
 done
