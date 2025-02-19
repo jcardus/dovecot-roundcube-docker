2cb8f1f80ac0e64707edc1c79a320ec9ee942f1a
jcardus <asklocation.net@gmail.com>
2025-02-19 00:54:02 +0000

    email

--- roundcube/config.php
+++ roundcube/config.php
@@ -20,19 +20,5 @@ $config['oidc_scope'] = 'openid email';
 
 // -------------- User Fields -----------------
 // Field for login UID. This may be an email ID
-$config['oidc_field_uid'] = 'mail';
-
-// Field for cleartext password
-$config['oidc_field_password'] = 'password';
-
-// Field for IMAP server
-$config['oidc_field_server'] = 'imap_server';
-
-// Alternative login page
-// This page gets included on login page
-// Any errors will be reported as $ERROR
-$config['oidc_login_page'] = '';
-
-$config['oidc_log_enabled'] = true;  // Enable plugin-specific logging
-
+$config['oidc_field_uid'] = 'email';
 
